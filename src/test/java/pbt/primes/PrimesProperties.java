318ecffa436ea43c2bea0869769700c38370514d
Johannes Link <business@johanneslink.net>
2021-02-22 14:57:57 +0100

    Disable test in PrimesProperties

--- src/test/java/pbt/primes/PrimesProperties.java
+++ src/test/java/pbt/primes/PrimesProperties.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.*;
 
 class PrimesProperties {
 
-	@Example
+	//@Example
 	void just2() {
 		List<Integer> factors = Primes.factorize(2);
 		assertThat(factors).containsExactly(2);
